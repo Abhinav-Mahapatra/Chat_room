56a951269b5d51489f42065473a3e9b59e1badc5
Abhinav-Mahapatra <abhinav.mahapatra10@gmail.com>
2018-04-10 13:38:10 +0530

    Add files via upload

--- Client.py
+++ Client.py
@@ -2,7 +2,7 @@
 """Script for Tkinter GUI chat client."""
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
-import tkinter
+import Tkinter
 
 from Translate import encode, decode
 
@@ -12,7 +12,7 @@ def receive():
     while True:
         try:
             msg = client_socket.recv(BUFSIZ).decode("utf8")
-            msg_list.insert(tkinter.END, msg)
+            msg_list.insert(Tkinter.END, msg)
         except OSError:  # Possibly client has left the chat.
             break
 
@@ -32,24 +32,24 @@ def on_closing(event=None):
     my_msg.set("{quit}")
     send()
 
-top = tkinter.Tk()
+top = Tkinter.Tk()
 top.title("Chatter")
 
-messages_frame = tkinter.Frame(top)
-my_msg = tkinter.StringVar()  # For the messages to be sent.
+messages_frame = Tkinter.Frame(top)
+my_msg = Tkinter.StringVar()  # For the messages to be sent.
 my_msg.set("Type your messages here.")
-scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
+scrollbar = Tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
 # Following will contain the messages.
-msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
-scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
-msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+msg_list = Tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+scrollbar.pack(side=Tkinter.RIGHT, fill=Tkinter.Y)
+msg_list.pack(side=Tkinter.LEFT, fill=Tkinter.BOTH)
 msg_list.pack()
 messages_frame.pack()
 
-entry_field = tkinter.Entry(top, textvariable=my_msg)
+entry_field = Tkinter.Entry(top, textvariable=my_msg)
 entry_field.bind("<Return>", send)
 entry_field.pack()
-send_button = tkinter.Button(top, text="Send", command=send)
+send_button = Tkinter.Button(top, text="Send", command=send)
 send_button.pack()
 
 top.protocol("WM_DELETE_WINDOW", on_closing)
@@ -75,4 +75,4 @@ client_socket.connect(ADDR)
 
 receive_thread = Thread(target=receive)
 receive_thread.start()
-tkinter.mainloop()  # Starts GUI execution.
+Tkinter.mainloop()  # Starts GUI execution.
